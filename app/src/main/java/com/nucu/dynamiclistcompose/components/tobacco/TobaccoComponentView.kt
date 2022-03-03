5b153bdd7389dd131ec06b1db61114cdb766100a
javipena <francisco.pena@rappi.com>
2022-03-02 21:22:39 -0600

    Dynamic list add more component to test performance

--- app/src/main/java/com/nucu/dynamiclistcompose/components/tobacco/TobaccoComponentView.kt
+++ app/src/main/java/com/nucu/dynamiclistcompose/components/tobacco/TobaccoComponentView.kt
@@ -9,9 +9,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.nucu.dynamiclistcompose.R
 
 @Composable
 fun TobaccoComponentView(
@@ -24,10 +26,10 @@ fun TobaccoComponentView(
     ) {
         Text(
             modifier = Modifier
-                .background(Color.Gray)
+                .background(Color.LightGray)
                 .padding(10.dp),
             textAlign = TextAlign.Justify,
-            text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+            text = stringResource(R.string.label_lorem_ipsum)
         )
     }
 }
