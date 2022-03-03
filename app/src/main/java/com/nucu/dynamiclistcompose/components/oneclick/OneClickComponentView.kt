4414ab73933a98b39d094bd95db8967bfdb3da68
javipena <francisco.pena@rappi.com>
2022-03-02 21:30:30 -0600

    Dynamic list add more component to test performance

--- app/src/main/java/com/nucu/dynamiclistcompose/components/oneclick/OneClickComponentView.kt
+++ app/src/main/java/com/nucu/dynamiclistcompose/components/oneclick/OneClickComponentView.kt
@@ -7,10 +7,12 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
@@ -18,6 +20,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
@@ -46,23 +49,25 @@ fun OneClickReorderComponentView(
                 Card(
                     modifier = Modifier
                         .width(200.dp)
-                        .height(100.dp),
+                        .wrapContentHeight(),
                     shape = RoundedCornerShape(16.dp),
                     elevation = 16.dp
                 ) {
                     Column(
                         modifier = Modifier
                             .clickable { onClick.invoke(it) }
-                            .padding(10.dp)
+                            .padding(10.dp),
+                        verticalArrangement = Arrangement.spacedBy(10.dp)
                     ) {
                         Text(text = date)
 
                         Row(
                             horizontalArrangement = Arrangement.spacedBy(10.dp)
                         ) {
-                            Image(modifier = Modifier.size(24.dp), painter = painterResource(id = R.drawable.ic_launcher_background), contentDescription = "")
-                            Image(modifier = Modifier.size(24.dp), painter = painterResource(id = R.drawable.ic_launcher_background), contentDescription = "")
-                            Image(modifier = Modifier.size(24.dp), painter = painterResource(id = R.drawable.ic_launcher_background), contentDescription = "")
+                            val size = 36.dp
+                            Image(modifier = Modifier.size(size), painter = painterResource(id = R.drawable.ic_launcher_background), contentDescription = "")
+                            Image(modifier = Modifier.size(size), painter = painterResource(id = R.drawable.ic_launcher_background), contentDescription = "")
+                            Image(modifier = Modifier.size(size), painter = painterResource(id = R.drawable.ic_launcher_background), contentDescription = "")
 
                             if (data.count() > MAX_ELEMENTS) {
                                 Box(
@@ -71,7 +76,7 @@ fun OneClickReorderComponentView(
                                     Text(text = "+${data.count() - MAX_ELEMENTS}")
                                 }
                             } else {
-                                Image(painter = painterResource(id = R.drawable.ic_launcher_foreground), contentDescription = "")
+                                Image(modifier = Modifier.size(size), painter = painterResource(id = R.drawable.ic_launcher_foreground), contentDescription = "")
                             }
                         }
                     }
