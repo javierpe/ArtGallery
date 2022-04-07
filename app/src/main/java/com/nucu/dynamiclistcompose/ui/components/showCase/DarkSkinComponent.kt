3245ad1c52dc09a2b8944c0d3f5ccca96e1d1afa
javipena <francisco.pena@rappi.com>
2022-04-06 22:46:39 -0500

    Tooltips

--- app/src/main/java/com/nucu/dynamiclistcompose/ui/components/showCase/DarkSkinComponent.kt
+++ app/src/main/java/com/nucu/dynamiclistcompose/ui/components/showCase/DarkSkinComponent.kt
@@ -4,12 +4,9 @@ import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
