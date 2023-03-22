044cdbcfb9db546182bd0c004a1ba85b6a8272c6
Francisco Javier Peña Ramos <francisco.javier.p.ramos@gmail.com>
2023-03-21 21:15:12 -0500

    Proguard keep data class

--- app/src/main/java/com/nucu/dynamiclistcompose/presentation/components/common/BannerImageView.kt
+++ app/src/main/java/com/nucu/dynamiclistcompose/presentation/components/common/BannerImageView.kt
@@ -1,19 +1,23 @@
 package com.nucu.dynamiclistcompose.presentation.components.common
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.nucu.dynamiclistcompose.presentation.components.banner.BannerInfo
 import com.nucu.dynamiclistcompose.presentation.components.bannerCarousel.BANNER_CAROUSEL_IMAGE_TEST_TAG
-
-const val FADE_IMAGE_DURATION = 1000
+import com.nucu.dynamiclistcompose.presentation.ui.theme.Typography
 
 @Composable
 fun BannerImageView(
@@ -46,6 +50,18 @@ fun BannerImageView(
             )
         }
 
-        Text(text = quantity.toString())
+        if (quantity > 0) {
+            Text(
+                modifier = Modifier
+                    .align(Alignment.TopEnd)
+                    .size(40.dp)
+                    .clip(RoundedCornerShape(bottomStart = 10.dp))
+                    .background(Color.White),
+                text = quantity.toString(),
+                style = Typography.h6,
+                color = Color.Black,
+                textAlign = TextAlign.Center
+            )
+        }
     }
 }
