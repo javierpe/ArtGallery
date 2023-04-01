cb694eb79f24d0273d2955c4010d6134e6f0c704
Francisco Javier Peña Ramos <francisco.javier.p.ramos@gmail.com>
2023-03-31 19:02:18 -0500

    Deeplink

--- navigation/api/src/main/java/com/javi/navigation/api/NavigationDestinationsApi.kt
+++ navigation/api/src/main/java/com/javi/navigation/api/NavigationDestinationsApi.kt
@@ -10,17 +10,34 @@ interface NavigationDestinationsApi {
      */
     fun setUp(navController: NavController)
 
+    /**
+     * Return to back
+     */
     fun popBackStack()
 
+    /**
+     * Navigate to product detail page
+     * @param launchSingle
+     * @param imageURL
+     */
     fun navigateToProductDetailPage(
         launchSingle: Boolean = false,
         imageURL: String
     )
 
+    /**
+     * Navigate to cards page.
+     * @param id of the cards container
+     * @param title of the cards
+     */
     fun navigateToCardsPage(
         id: Int,
         title: String
     )
 
+    /**
+     * Navigate to any destination like deeplink.
+     * @param uri like 'cards_page/1/Cactus/'
+     */
     fun navigate(uri: Uri)
 }
